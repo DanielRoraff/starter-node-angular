b0e662400ef61a2c1d6fda2947fd1413b447b165
Daniel <danielroraff@gmail.com>
2021-04-12 20:25:36 -0700

    changes for days

--- deploy.js
+++ deploy.js
@@ -85,9 +85,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.90.65.193',
+      host: '00.00.00.00',
       username: 'ubuntu',
-      privateKey: 'node-key.pem'
+      privateKey: 'insert key'
     })
     .then(function() {
       console.log('SSH Connection established.');
